f1c1bf4881e777f0d1ef3c860469cf4de2aecf5d
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-05-25 17:12:21 -0400

    Este es el primer commit

---
+++ chatbot_main.cpp
@@ -0,0 +1,80 @@
+/* PROYECTO CHATBOT 
+	Chatbot para centro comercial grande, el cual proporcionara inforamacion 
+	sobre la ubicacion de las tiendas, cine, baños, parqueos, etc.; tambien 
+	se proporcionara alguna informacion adicional sobre lo que se pregunte.
+*/
+
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include <vector>
+using namespace std;
+#define t 4
+vector<string> separar(string str);
+void tiendas();
+void cines();
+
+int main(){
+    string chatU;
+    vector<string> palabra;
+    // Palabras
+    string tienda[t] = {"tienda", "tiendas","Tienda", "Tiendas"};
+    string cine[t] = {"cine", "cines","Cines", "Cine"};
+    
+    cout<<"Hola, en que puedo ayudarte? ";
+    getline(cin, chatU);
+    
+    while((chatU != "salir") && (chatU != "Salir")){
+    	
+    	palabra = separar(chatU);
+    
+    	for(int i = 0; i < palabra.size(); i++){
+        	for(int p=0; p < t; p++){
+        		
+        		if(palabra[i] == tienda[p]){	
+    				tiendas(); 
+				} else if (palabra[i] == cine[p]){
+					cines();
+				}
+				
+			} 	
+    	}
+    	// Salida del bucle
+    	cout<<"\n\nEn que otra cosa puedo ayudarte? ";
+    	getline(cin, chatU);
+	}
+    
+    
+		
+	cout<<"Fin del programa!";
+	getch();
+	return 0;
+}
+
+
+// Separ las palabra y metarlas en un vector
+vector<string> separar(string str) {
+    char cortar = ' ';
+    int posInicial = 0;
+    int posEncontrada = 0;
+    string separado;
+    vector<string> palabras;
+    
+    while(posEncontrada >= 0){
+    	// Es justo donde lo encuetra; me da negatvo si no la encuentra ??
+        posEncontrada = str.find(cortar, posInicial); // 5
+        // No altera la cadena introducida, CREA UNA NUEVA -1
+        separado = str.substr(posInicial, posEncontrada - posInicial); // (0, 5-0 = 5); 
+        posInicial = posEncontrada + 1;
+        palabras.push_back(separado);
+    }
+    
+    return palabras;
+}
+
+void tiendas(){
+	cout<<"Informacion sobre tiendas"<<endl;
+}
+void cines(){
+	cout<<"Informacion sobre cines"<<endl;
+}
