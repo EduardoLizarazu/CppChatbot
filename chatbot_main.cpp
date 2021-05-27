c8d1ae7d5f843a37e99172189b68f096ff4c3ad0
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-05-27 17:15:30 -0400

    Añadido de matriz y lectura de la misma

--- chatbot_main.cpp
+++ chatbot_main.cpp
@@ -14,8 +14,8 @@ int main(){
     string chatU;
     vector<string> palabra;
 
-    string tienda[t] = {"tienda", "tiendas","Tienda", "Tiendas"};
-    string cine[t] = {"cine", "cines","Cines", "Cine"};
+    string tienda[t] = {"Tiendas", "tiendas","Tienda", "tienda"};
+    string cine[t] = {"Cines", "cines","Ciene", "Cine"};
     
     cout<<"Hola, en que puedo ayudarte? ";
     getline(cin, chatU); // Entrada
@@ -30,17 +30,14 @@ int main(){
     				tiendas(); 
 				} else if (palabra[i] == cine[p]){
 					cines();
+					
 				}			
 			} 	
     	}
-    	  
     	// Salida
     	cout<<"\n\nEn que otra cosa puedo ayudarte? ";
     	getline(cin, chatU);
 	}
-    
-    
-		
 	cout<<"Fin del programa!";
 	getch();
 	return 0;
@@ -56,20 +53,35 @@ vector<string> separar(string str) {
     vector<string> palabras;
     
     while(posEncontrada >= 0){
-    	// Es justo donde lo encuetra; me da negatvo si no la encuentra ??
+    	// Es justo donde lo encuetra
         posEncontrada = str.find(cortar, posInicial); // 5
         // No altera la cadena introducida, CREA UNA NUEVA -1
         separado = str.substr(posInicial, posEncontrada - posInicial); // (0, 5-0 = 5); 
         posInicial = posEncontrada + 1;
         palabras.push_back(separado);
     }
-    
     return palabras;
 }
 
 void tiendas(){
-	cout<<"Informacion sobre tiendas"<<endl;
+	string nombreTiendas[3][2] = {
+		{"Tienda-1 =", " Ubicacion-1"},
+		{"Tienda-2 =", " Ubicacion-2"},
+		{"Tienda-3 =", " Ubicacion-3"}
+	};
+	for(int i=0; i<3; i++){
+		for(int j=0; j<2; j++){
+			cout<<nombreTiendas[i][j];f
+		}
+		cout<<"\n";
+	}
 }
 void cines(){
 	cout<<"Informacion sobre cines"<<endl;
 }
+
+
+
+
+
+
