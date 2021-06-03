ce1345fc31f50bd91aa078190b65664222c3847a
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-06-03 15:38:40 -0400

    Añdido de comidas

--- chatbot_main.cpp
+++ chatbot_main.cpp
@@ -1,4 +1,4 @@
-
+ 
 #include<iostream>
 #include<conio.h>
 #include<string.h>
@@ -9,28 +9,28 @@ using namespace std;
 vector<string> separar(string str);
 void tiendas();
 void cines();
+void comidas();
 
 int main(){
     string chatU;
     vector<string> palabra;
 
     string tienda[t] = {"Tiendas", "tiendas","Tienda", "tienda"};
-    string cine[t] = {"Cines", "cines","Ciene", "Cine"};
-    
-    cout<<"Hola, en que puedo ayudarte? ";
+    string cine[t] = {"Cines", "cines","Cine", "cine"};
+    string comida[t] = {"Comidas", "comidas","Comida", "comida"};
+    cout<<"Hola, soy un bot que te indicara la ubicacion de tiendas, cines, patio de comidas"<<"\n"<<"¿En que puedo ayudarle?";
     getline(cin, chatU); // Entrada
     
-    while((chatU != "salir") && (chatU != "Salir")){
-    	
+    while((chatU != "salir") && (chatU != "Salir")){	
     	palabra = separar(chatU);
-    
     	for(int i = 0; i < palabra.size(); i++){
         	for(int p=0; p < t; p++){
         		if(palabra[i] == tienda[p]){	
     				tiendas(); 
 				} else if (palabra[i] == cine[p]){
-					cines();
-					
+					cines();				
+				} else if(palabra[i] == comida[p]){
+					comidas();
 				}			
 			} 	
     	}
@@ -44,7 +44,7 @@ int main(){
 }
 
 
-// Separ las palabra y metarlas en un vector
+// Separ las palabra y metarlas en un vector con push (inspirada en python)
 vector<string> separar(string str) {
     char cortar = ' ';
     int posInicial = 0;
@@ -64,21 +64,47 @@ vector<string> separar(string str) {
 }
 
 void tiendas(){
-	string nombreTiendas[3][2] = {
+	string nombreTiendas[4][2] = {
 		{"Tienda-1 =", " Ubicacion-1"},
 		{"Tienda-2 =", " Ubicacion-2"},
-		{"Tienda-3 =", " Ubicacion-3"}
+		{"Tienda-3 =", " Ubicacion-3"},
+		{"Tienda-4 =", " Ubicacion-4"}
 	};
-	for(int i=0; i<3; i++){
+	for(int i=0; i<4; i++){
 		for(int j=0; j<2; j++){
-			cout<<nombreTiendas[i][j];f
+			cout<<nombreTiendas[i][j];
 		}
 		cout<<"\n";
 	}
 }
+
 void cines(){
-	cout<<"Informacion sobre cines"<<endl;
+	string nombreCines[2][2] = {
+		{"Cine-1 =", " Ubicacion-1"},
+		{"Cine-2 =", " Ubicacion-2"}
+	};
+	for(int i=0; i<2; i++){
+		for(int j=0; j<2; j++){
+			cout<<nombreCines[i][j];
+		}
+		cout<<"\n";
+	}
 }
+void comidas(){
+	string nombreComida[4][2] = {
+		{"Patio-1 =", " Ubicacion-1"},
+		{"Patio-2 =", " Ubicacion-2"},
+		{"Patio-3 =", " Ubicacion-3"},
+		{"Patio-4 =", " Ubicacion-4"}
+	};
+	for(int i=0; i<4; i++){
+		for(int j=0; j<2; j++){
+			cout<<nombreComida[i][j];
+		}
+		cout<<"\n";
+	}
+}
+
 
 
 
